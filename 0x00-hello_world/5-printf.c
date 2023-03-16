bb16445cac3361cc9bb5638b3bc8e6c0f63ee449
Mamoswanyane <mamoswanyanedinah@gmail.com>
2023-03-16 03:44:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stuio.h>
+/**
+ * main - A programme that print a line using the printf functiun
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (o);
+} 
